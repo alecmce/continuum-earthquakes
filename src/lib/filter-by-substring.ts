e7cd7bf3ae2d214d1eca2fc96c77f7ff8b04efea
Alec McEachran <alecmce@gmail.com>
2024-06-15 12:12:20 +0100

    Add documentation to filter-by-substring.ts

--- src/lib/filter-by-substring.ts
+++ src/lib/filter-by-substring.ts
@@ -4,6 +4,10 @@ interface Props<T> {
   substring: string
 }
 
+/**
+ * Returns a filtered array of items, returns a function for extracting a string from that item and a
+ * substring that must be contained in that name.
+ */
 export function filterBySubstring<T>(props: Props<T>): T[] {
   const { getString, source, substring } = props
 
